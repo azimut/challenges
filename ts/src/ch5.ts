1e3c4fede78808ff4ebf5574cbc4c23f3834c689
azimut <azimut.github@protonmail.com>
2022-07-19 22:48:00 -0300

    ts5

---
+++ ts/src/ch5.ts
@@ -0,0 +1,27 @@
+import { assert } from 'chai';
+
+// https://www.codewars.com/kata/55c45be3b2079eccff00010f/train/typescript
+
+// Your task is to sort a given string. Each word in the string will contain a single number. This number is the
+// position the word should have in the result.
+
+// Note: Numbers can be from 1 to 9. So 1 will be the first word (not 0).
+
+// If the input string is empty, return an empty string. The words in the input String will only contain valid
+// consecutive numbers.
+
+export function order(words: string): string {
+  const toNumber = (s: string) => Number(s.replace(/\D/g, ''));
+  return words
+    .split(' ')
+    .sort((a, b) => toNumber(a) - toNumber(b))
+    .join(' ');
+}
+
+describe('solution', function () {
+  it('basicTests', function () {
+    assert.equal(order('is2 Thi1s T4est 3a'), 'Thi1s is2 3a T4est');
+    assert.equal(order('4of Fo1r pe6ople g3ood th5e the2'), 'Fo1r the2 g3ood 4of th5e pe6ople');
+    assert.equal(order(''), '');
+  });
+});
