3177bb2f08ee2b637fe24222b8c4c730501e9c7e
azimut <azimut.github@protonmail.com>
2022-12-03 19:39:41 -0300

    aoc: cleanup day3, use helper, add dune release

--- ocaml/aoc2022/day3.ml
+++ ocaml/aoc2022/day3.ml
@@ -1,25 +1,24 @@
 open Batteries
+open Aoc
 
-(* silver test = 157 *)
+let input_file = Aoc.arg_or "day3.txt"
 
 let priority item =
-  match (Char.is_letter item, Char.is_uppercase item) with
-  | true, true -> Char.code item - 38
-  | true, false -> Char.code item - 96
-  | false, _ -> failwith "got a non letter!?"
+  match Char.is_uppercase item with
+  | true -> Char.code item - 38
+  | false -> Char.code item - 96
 
-let split2 l = List.(split_at (length l / 2) l)
-let intersect2 (l1, l2) = Set.(intersect (of_list l1) (of_list l2) |> to_list)
+let split2 lst = List.(split_at (length lst / 2) lst)
+let intersect2 (l1, l2) = Set.(intersect (of_list l1) (of_list l2) |> any)
 
+(* silver test = 157 *)
 let silver =
-  File.lines_of "day3.txt"
-  |> List.of_enum
-  |> List.map String.explode
-  |> List.map split2
-  |> List.map intersect2
-  |> List.flatten
-  |> List.map priority
-  |> List.reduce ( + )
+  File.lines_of input_file
+  |> Enum.map String.explode
+  |> Enum.map split2
+  |> Enum.map intersect2
+  |> Enum.map priority
+  |> Enum.reduce ( + )
 
 (* https://stackoverflow.com/q/48713886 *)
 let group_by_3 lst =
@@ -31,18 +30,20 @@ let group_by_3 lst =
   let groups, last, _ = List.fold_left f accum lst in
   List.rev (List.rev last :: groups)
 
-let intersect3 [ l1; l2; l3 ] =
-  Set.(intersect (of_list l1) (of_list l2) |> intersect (of_list l3) |> to_list)
+let intersect3 = function
+  | [ l1; l2; l3 ] ->
+      Set.(intersect (of_list l1) (of_list l2) |> intersect (of_list l3) |> any)
+  | _ -> failwith "wrong shape"
 
 (* gold test = 70 *)
 let gold =
-  File.lines_of "day3.txt"
+  File.lines_of input_file
   |> List.of_enum
   |> List.map String.explode
   |> group_by_3
   |> List.map intersect3
-  |> List.flatten
   |> List.map priority
   |> List.reduce ( + )
 
+(* let main = Printf.printf "gold:\t%10d\n" silver *)
 let main = Printf.printf "silver:\t%10d\ngold:\t%10d\n" silver gold
