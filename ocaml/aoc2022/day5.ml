c779e076d3c92ee1037a9b4af292d611fe393942
azimut <azimut.github@protonmail.com>
2022-12-05 18:35:33 -0300

    aoc day5 actually parse input

--- ocaml/aoc2022/day5.ml
+++ ocaml/aoc2022/day5.ml
@@ -1,11 +1,11 @@
 open! Batteries
 
-let input_file = Aoc.arg_or "day5.test.txt"
+let input_file = Aoc.arg_or "day5.txt"
 
 type movement = { qty : int; src : int; dst : int }
 
-let movements file =
-  File.with_file_in file IO.read_all
+let movements =
+  File.with_file_in input_file IO.read_all
   |> String.split_on_string ~by:"\n\n"
   |> List.last
   |> String.trim
@@ -14,25 +14,26 @@ let movements file =
          Scanf.sscanf s "move %d from %d to %d" (fun qty src dst ->
              { qty; src = src - 1; dst = dst - 1 }))
 
-let input_stacks () =
-  match input_file with
-  | "day5.txt" ->
-      [
-        [ 'H'; 'B'; 'V'; 'W'; 'N'; 'M'; 'L'; 'P' ];
-        [ 'M'; 'Q'; 'H' ];
-        [ 'N'; 'D'; 'B'; 'G'; 'F'; 'Q'; 'M'; 'L' ];
-        [ 'Z'; 'T'; 'F'; 'Q'; 'M'; 'W'; 'G' ];
-        [ 'M'; 'T'; 'H'; 'P' ];
-        [ 'C'; 'B'; 'M'; 'J'; 'D'; 'H'; 'G'; 'T' ];
-        [ 'M'; 'N'; 'B'; 'F'; 'V'; 'R' ];
-        [ 'P'; 'L'; 'H'; 'M'; 'R'; 'G'; 'S' ];
-        [ 'P'; 'D'; 'B'; 'C'; 'N' ];
-      ]
-      |> List.map (fun lst -> lst |> List.enum |> Stack.of_enum)
-  | "day5.test.txt" ->
-      [ [ 'Z'; 'N' ]; [ 'M'; 'C'; 'D' ]; [ 'P' ] ]
-      |> List.map (fun lst -> lst |> List.enum |> Stack.of_enum)
-  | _ -> failwith "unknown input ????"
+let input_stacks file =
+  let mod4 n = Int.modulo n 4 in
+  let lines =
+    File.with_file_in file IO.read_all
+    |> String.split_on_string ~by:"\n\n"
+    |> List.first
+    |> String.split_on_char '\n'
+    |> List.rev
+  in
+  let stacks =
+    List.init
+      ((String.length (List.first lines) / 4) + 1)
+      (fun _ -> Stack.create ())
+  in
+  lines
+  |> List.iter
+       (String.iteri (fun i c ->
+            if mod4 (i - 1) = 0 && Char.is_letter c then
+              Stack.push c (List.at stacks (i / 4))));
+  stacks
 
 let popper stack n =
   let ret = ref [] in
@@ -43,9 +44,9 @@ let popper stack n =
 
 let pusher stack = List.iter (fun e -> Stack.push e stack)
 
-let silver () =
-  let stacks = input_stacks () in
-  movements input_file
+let silver =
+  let stacks = input_stacks input_file in
+  movements
   |> List.iter (fun m ->
          let src_stack = List.at stacks m.src in
          let dst_stack = List.at stacks m.dst in
@@ -55,9 +56,9 @@ let silver () =
   |> List.map (fun s -> if Stack.length s = 0 then ' ' else Stack.top s)
   |> String.implode
 
-let gold () =
-  let stacks = input_stacks () in
-  movements input_file
+let gold =
+  let stacks = input_stacks input_file in
+  movements
   |> List.iter (fun m ->
          let src_stack = List.at stacks m.src in
          let dst_stack = List.at stacks m.dst in
@@ -66,3 +67,5 @@ let gold () =
   stacks
   |> List.map (fun s -> if Stack.length s = 0 then ' ' else Stack.top s)
   |> String.implode
+
+let _ = Printf.printf "silver:\t%s\ngold:\t%s\n" silver gold
