7cb9d43f9b34d1c7bf6bcc793b1dda3330e8b954
azimut <azimut.github@protonmail.com>
2022-12-06 13:16:10 -0300

    aoc minor day1 cleanup

--- ocaml/aoc2022/day1.ml
+++ ocaml/aoc2022/day1.ml
@@ -1,19 +1,16 @@
 open Batteries
 
-let silver =
-  File.with_file_in "aoc2022/day1.txt" IO.read_all
+let input =
+  File.with_file_in "day1.txt" IO.read_all
   |> String.split_on_string ~by:"\n\n"
+  |> List.map String.trim
   |> List.map (String.split_on_char '\n')
-  |> List.map (List.map (fun s -> try int_of_string s with _ -> 0))
+  |> List.map (List.map int_of_string)
   |> List.map (List.reduce ( + ))
-  |> List.max
+
+let silver = input |> List.max
 
 let gold =
-  File.with_file_in "aoc2022/day1.txt" IO.read_all
-  |> String.split_on_string ~by:"\n\n"
-  |> List.map (String.split_on_char '\n')
-  |> List.map (List.map (fun s -> try int_of_string s with _ -> 0))
-  |> List.map (List.reduce ( + ))
-  |> List.sort compare |> List.rev |> List.take 3 |> List.reduce ( + )
+  input |> List.sort compare |> List.rev |> List.take 3 |> List.reduce ( + )
 
 let main = Printf.printf "silver: %d gold: %d\n" silver gold
