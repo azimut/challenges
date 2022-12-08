c39e11b0c842354b25dd50390355d22281e995c9
azimut <azimut.github@protonmail.com>
2022-12-07 23:43:27 -0300

    aoc day7 minor, sorting stuff

--- ocaml/aoc2022/day7.ml
+++ ocaml/aoc2022/day7.ml
@@ -7,23 +7,13 @@ open! Batteries
 
 type dirtree = File of string * int | Dir of string * dirtree list * int
 
-module Cwd = struct
-  type t = string Stack.t
-
-  let create = Stack.create
-  let pushd = Stack.push
-  let popd = Stack.pop
-
-  let pwd cwd =
-    Stack.enum cwd |> List.of_enum |> List.rev |> String.join "/" |> function
-    | "/" -> "/"
-    | s -> String.lchop s
-
-  let ls cwd path =
-    match pwd cwd with
-    | "/" -> Printf.sprintf "/%s" path
-    | s -> Printf.sprintf "%s/%s" s path
-end
+(* NOTE: day7.txt has repeated directory names *)
+(* NOTE: this works because I know logs cd's to something meaninful before ls *)
+let parsed =
+  File.with_file_in "day7.txt" IO.read_all
+  |> String.trim
+  |> String.split_on_char '\n'
+  |> List.filter (( <> ) "$ ls")
 
 let rec map f = function
   | Dir (dirname, x :: xs, size) ->
@@ -44,39 +34,44 @@ let insert' at_dir new_dt = function
   | File _ as file -> file
 
 let insert at_dir new_dt = map (insert' at_dir new_dt)
-let read_cd s = Scanf.sscanf s "$ cd %s" Fun.id
-let read_dir cwd s = Dir (Scanf.sscanf s "dir %s" (fun s -> Cwd.ls cwd s), [], 0)
-let read_file s = Scanf.sscanf s "%d %s" (fun size name -> File (name, size))
 
-let btest accumulator = function
-  | File (_, size) -> size + accumulator
-  | Dir _ -> accumulator
+module Cwd = struct
+  type t = string Stack.t
 
-let update_dir_size' = function
-  | Dir (name, subdirs, _) as dir -> Dir (name, subdirs, fold btest 0 dir)
-  | File _ as f -> f
+  let create = Stack.create
+  let pushd = Stack.push
+  let popd = Stack.pop
 
-let init_immediate_dirsizes = map update_dir_size'
+  let pwd cwd =
+    Stack.enum cwd |> List.of_enum |> List.rev |> String.join "/" |> function
+    | "/" -> "/"
+    | s -> String.lchop s
 
-let sum_smallish_dirs =
-  fold
-    (fun acc dt ->
-      match dt with
-      | File _ -> acc
-      | Dir (_, _, size) when size <= 100000 -> acc + size
-      | Dir _ -> acc)
-    0
-
-(* NOTE: day7.txt has repeated directory names *)
-(* NOTE: this works because I know logs cd's to something meaninful before ls *)
-let parsed =
-  File.with_file_in "day7.txt" IO.read_all
-  |> String.trim
-  |> String.split_on_char '\n'
-  |> List.filter (( <> ) "$ ls")
+  let ls cwd path =
+    match pwd cwd with
+    | "/" -> Printf.sprintf "/%s" path
+    | s -> Printf.sprintf "%s/%s" s path
+end
 
 let directory_tree =
+  let sum_filesizes accumulator = function
+    | File (_, size) -> size + accumulator
+    | Dir _ -> accumulator
+  in
+  let update_dir_size' = function
+    | Dir (name, subdirs, _) as dir ->
+        Dir (name, subdirs, fold sum_filesizes 0 dir)
+    | File _ as f -> f
+  in
+  let init_immediate_dirsizes = map update_dir_size' in
   let cwd = Cwd.create () in
+  let read_cd s = Scanf.sscanf s "$ cd %s" Fun.id in
+  let read_dir cwd s =
+    Dir (Scanf.sscanf s "dir %s" (fun s -> Cwd.ls cwd s), [], 0)
+  in
+  let read_file s =
+    Scanf.sscanf s "%d %s" (fun size name -> File (name, size))
+  in
   let rec silver' cwd dt lst =
     match lst with
     | "$ cd /" :: xs ->
@@ -97,6 +92,15 @@ let directory_tree =
     | _ -> assert false
   in
   silver' cwd (Dir ("/", [], 0)) parsed |> init_immediate_dirsizes
+
+let sum_smallish_dirs =
+  fold
+    (fun acc dt ->
+      match dt with
+      | File _ -> acc
+      | Dir (_, _, size) when size <= 100000 -> acc + size
+      | Dir _ -> acc)
+    0
 
 let silver () = directory_tree |> sum_smallish_dirs
 
