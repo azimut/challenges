08b353da3b6174ad9d364dadd961468cde952c47
azimut <azimut.github@protonmail.com>
2023-04-05 20:49:34 -0300

    algoexpert: add "performance test" comparison

--- java/algoexpert/TwoSum.java
+++ java/algoexpert/TwoSum.java
@@ -1,6 +1,17 @@
 import java.util.*;
 
 // 01. Two Number Sum.mp4
+//
+// 100_000 random array // target sum = 1_000
+//
+// Linear: [1940665280, -1940664280]
+// System execution time: 13251
+//
+// Set: [230282379, -230282369]
+// System execution time: 20
+//
+// Sorted: [-1940664280, 1940665280]
+// System execution time: 33
 
 class TwoSum
 {
@@ -9,10 +20,10 @@ class TwoSum
 
     // O(n^2)
     private static String linearSearch(int[] arr, int targetSum) {
-        for (int i = 0; i < arr.length-2; i++) {
-            for (int j = i+1; j < arr.length-1; j++) {
+        for (int i = 0; i < arr.length-1; i++) {
+            for (int j = i+1; j < arr.length; j++) {
                 if (arr[i] + arr[j] == targetSum) {
-                    return String.format("Found `%d` and `%d` sum to `%d`", arr[i], arr[j], targetSum);
+                    return String.format("[%d, %d]", arr[i], arr[j]);
                 }
             }
         }
@@ -25,7 +36,7 @@ class TwoSum
         for (int i = 0; i < arr.length-1; i++) {
             int target = 10 - arr[i];
             if (s.contains(target)) {
-                return String.format("Found `%d` and `%d` sum to `%d`", arr[i], target, targetSum);
+                return String.format("[%d, %d]", arr[i], target);
             }
             else {
                 s.add(arr[i]);
@@ -42,14 +53,10 @@ class TwoSum
         while (true) {
             int currentSum = arr[left] + arr[right];
             if (currentSum == targetSum) {
-                return String.format("Found `%d` and `%d` sum to `%d`", arr[left], arr[right], targetSum);
+                return String.format("[%d, %d]", arr[left], arr[right]);
             }
-            if (currentSum > targetSum) {
-                right--;
-            }
-            if (currentSum < targetSum) {
-                left++;
-            }
+            if (currentSum > targetSum) right--;
+            if (currentSum < targetSum) left++;
             if (left >= right) {
                 break;
             }
@@ -59,11 +66,31 @@ class TwoSum
 
     public static void main(String[] args)
     {
-        System.out.println(arr[0]);
+        System.out.println(Arrays.toString(arr));
         System.out.println("Linear: " + linearSearch(arr, targetSum));
         System.out.println("Set: "    + setSearch(arr, targetSum));
-        System.out.println(Arrays.toString(arr));
         System.out.println("Sorted: " + sortedSearch(arr, targetSum));
         System.out.println(Arrays.toString(arr));
+        Random rd = new Random();
+        int[] bar = new int[100000];
+        for (int i = 0; i < bar.length; i++) {
+            bar[i] = rd.nextInt();
+        }
+        long startTime;
+        long endTime;
+        startTime = System.currentTimeMillis();
+        System.out.println("Linear: " + linearSearch(bar, 1000));
+        endTime = System.currentTimeMillis();
+        System.out.println("System execution time: " + (endTime - startTime));
+
+        startTime = System.currentTimeMillis();
+        System.out.println("Set: "    + setSearch(bar, 1000));
+        endTime = System.currentTimeMillis();
+        System.out.println("System execution time: " + (endTime - startTime));
+
+        startTime = System.currentTimeMillis();
+        System.out.println("Sorted: " + sortedSearch(bar, 1000));
+        endTime = System.currentTimeMillis();
+        System.out.println("System execution time: " + (endTime - startTime));
     }
 }
