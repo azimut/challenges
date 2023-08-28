bcabeae05c1392f6134572bb25e4680f94193d8c
azimut <azimut.github@protonmail.com>
2023-08-28 07:52:35 -0300

    algoexpert: cleanup easy/11/c

--- algoexpert/easy/11.three_largest.c
+++ algoexpert/easy/11.three_largest.c
@@ -4,17 +4,12 @@
 int main(void) {
   int input[] = {141, 1, 17, -7, -17, -27, 18, 541, 8, 7, 7};
   int largest[3];
-  int tmp;
   memset(largest, 0, sizeof(int) * 3);
   for (int i = 0; i < sizeof(input) / sizeof(int); ++i) {
     for (int j = 2; j >= 0; --j) {
       if (input[i] > largest[j]) {
-        // shift places
-        if (j > 0) {
-          tmp = largest[1];
-          largest[0] = tmp;
-          if (j == 2)
-            largest[1] = largest[2];
+        for (int k = 0; k < j; ++k) {
+          largest[k] = largest[k + 1];
         }
         largest[j] = input[i];
         break;
