650aa5b21902cb52710aef77c09f8942673735c0
azimut <azimut.github@protonmail.com>
2023-08-28 08:51:44 -0300

    algoexpert: dumb brace removal for easy/11/c

--- algoexpert/easy/11.three_largest.c
+++ algoexpert/easy/11.three_largest.c
@@ -4,17 +4,14 @@
 int main(void) {
   int input[] = {141, 1, 17, -7, -17, -27, 18, 541, 8, 7, 7};
   int largest[3] = {0};
-  for (int i = 0; i < sizeof(input) / sizeof(int); ++i) {
-    for (int j = 2; j >= 0; --j) {
+  for (int i = 0; i < sizeof(input) / sizeof(int); ++i)
+    for (int j = 2; j >= 0; --j)
       if (input[i] > largest[j]) {
-        for (int k = 0; k < j; ++k) {
+        for (int k = 0; k < j; ++k)
           largest[k] = largest[k + 1];
-        }
         largest[j] = input[i];
         break;
       }
-    }
-  }
   printf("[%d,%d,%d]\n", largest[0], largest[1], largest[2]);
   return 0;
 }
