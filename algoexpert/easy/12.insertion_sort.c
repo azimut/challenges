7177871a9450f46e34a8cb591a30f6c5b767567b
azimut <azimut.github@protonmail.com>
2023-08-29 01:05:48 -0300

    algoexpert: add easy/12/c

---
+++ algoexpert/easy/12.insertion_sort.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+void swap(int xs[], int i, int j) {
+  int tmp = xs[j];
+  xs[j] = xs[i];
+  xs[i] = tmp;
+}
+
+int main(void) {
+  int input[] = {8, 5, 2, 9, 5, 6, 3};
+  int length = sizeof(input) / sizeof(int);
+
+  for (int i = 1; i < length; ++i)
+    for (int j = i - 1; j >= 0; --j)
+      if (input[i] < input[j]) {
+        swap(input, i, j);
+        i = j;
+      }
+
+  for (int i = 0; i < length; ++i)
+    printf("%d: %d\n", i, input[i]);
+
+  return 0;
+}
