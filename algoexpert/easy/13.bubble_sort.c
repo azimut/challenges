7d01194bb9e3b40cd57b28117f88122969438189
azimut <azimut.github@protonmail.com>
2023-08-29 22:02:31 -0300

    algoexpert: might be true bubble sort easy/13/c

--- algoexpert/easy/13.bubble_sort.c
+++ algoexpert/easy/13.bubble_sort.c
@@ -1,21 +1,27 @@
 #include <stdbool.h>
 #include <stdio.h>
 
+void swap(int xs[], int i, int j) {
+  int tmp = xs[j];
+  xs[j] = xs[i];
+  xs[i] = tmp;
+}
+
 int main() {
   int numbers[] = {8, 5, 2, 9, 5, 6, 3};
-  int tmp, length = sizeof(numbers) / sizeof(int);
-  bool swapped;
-  for (int i = 0; i < length; ++i) {
-    swapped = false;
-    for (int j = i + 1; j < length; ++j)
-      if (numbers[i] > numbers[j]) {
-        tmp = numbers[j];
-        numbers[j] = numbers[i];
-        numbers[i] = tmp;
-        swapped = true;
-      }
-    if (!swapped)
-      break;
+  int length = sizeof(numbers) / sizeof(int);
+  int top = length;
+  bool swapped = false;
+  for (int i = 0; i < top - 1; ++i) {
+    if (numbers[i] > numbers[i + 1]) {
+      swap(numbers, i, i + 1);
+      swapped = true;
+    }
+    if (i == top - 2 && swapped) {
+      i = -1;
+      top--;
+      swapped = false;
+    }
   }
   for (int i = 0; i < length; ++i)
     printf("[%d] = %d\n", i, numbers[i]);
