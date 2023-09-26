4497bf83f1f27bb68ea9e20e3c6bc77f9cd20abd
azimut <azimut.github@protonmail.com>
2023-09-26 19:58:11 -0300

    algoexpert: add ocaml versions of medium bst 10

---
+++ algoexpert/medium/10.bst_min_height.ml
@@ -0,0 +1,40 @@
+type 'a tree =
+  | Leaf
+  | Node of 'a * 'a tree * 'a tree
+
+(* after hearing the explaination, before seeing code *)
+let middle = function
+  | []           -> None
+  | x :: _ :: [] -> Some x
+  | xs           -> Some List.(nth xs (length xs / 2))
+
+let left xs =
+  match middle xs with
+  | Some middle -> List.filter ((>) middle) xs
+  | None        -> []
+
+let right xs =
+  match middle xs with
+  | Some middle -> List.filter ((<) middle) xs
+  | None        -> []
+
+let rec min_height xs =
+  match middle xs with
+  | Some mid ->
+     Node (mid, min_height (left xs), min_height (right xs))
+  | None     ->
+     Leaf
+
+let flat_tree = [1;2;5;7;10;13;14;15;22]
+let possible_tree =
+  Node(10,
+       Node(2,
+            Node(1, Leaf, Leaf),
+            Node(5,
+                 Leaf,
+                 Node(7, Leaf, Leaf))),
+       Node(14,
+            Node(13, Leaf, Leaf),
+            Node(15,
+                 Leaf,
+                 Node(22, Leaf, Leaf))))
