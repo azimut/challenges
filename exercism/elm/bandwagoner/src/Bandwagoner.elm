93fad675aee179b63ef446e8bf0914d4de06d361
azimut <azimut.github@protonmail.com>
2023-12-03 19:00:19 -0300

    exercism: add elm "bandwagoner"

---
+++ exercism/elm/bandwagoner/src/Bandwagoner.elm
@@ -0,0 +1,26 @@
+module Bandwagoner exposing (..)
+
+type alias Coach =
+    { name : String
+    , formerPlayer : Bool
+    }
+
+type alias Stats =
+    { wins : Int
+    , losses : Int
+    }
+
+type alias Team =
+    { name : String
+    , coach : Coach
+    , stats : Stats
+    }
+
+createTeam : String -> Stats -> Coach -> Team
+createTeam name stats coach = Team name coach stats
+
+replaceCoach : Coach -> Team -> Team
+replaceCoach newCoach team = { team | coach = newCoach }
+
+rootForTeam : { a | stats : Stats } -> Bool
+rootForTeam { stats } = stats.wins > stats.losses
