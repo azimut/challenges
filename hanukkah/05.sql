6d11ccada42ff61b78c0f80d7094760ea08be60e
azimut <azimut.github@protonmail.com>
2024-05-11 10:41:52 -0300

    hanukkah: add 5 solution in sql

---
+++ hanukkah/05.sql
@@ -0,0 +1,11 @@
+-- Abigail Diaz, Marie Noble
+SELECT customers.name, customers.citystatezip, customers.phone, count(*) as foo
+  FROM orders
+       JOIN customers    ON orders.customerid = customers.customerid
+       JOIN orders_items ON orders.orderid    = orders_items.orderid
+       JOIN products     ON orders_items.sku  = products.sku
+ WHERE customers.citystatezip LIKE 'Staten Island%' AND
+       products.desc LIKE '%Senior Cat%'
+ GROUP BY customers.name
+ ORDER BY foo DESC
+ LIMIT 1;
