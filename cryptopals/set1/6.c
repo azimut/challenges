ab7b9969daa1438b4e58cc4f11fafe53dccc3d46
azimut <azimut.github@protonmail.com>
2025-11-21 16:55:58 -0300

    crytopals: 1.6 wip single key size guess

--- cryptopals/set1/6.c
+++ cryptopals/set1/6.c
@@ -11,12 +11,15 @@ int count_bits(const char letter) {
 }
 
 // TODO: account for missmatched lengths
-int hamming_distance(const char *a, const char *b) {
-  assert(strlen(a) == strlen(b));
+int hamming_distance_buffer(const Buffer a, const Buffer b) {
+  assert(a.size == b.size);
   int distance = 0;
-  for (size_t i = 0; i < strlen(a); ++i)
-    distance += count_bits(a[i] ^ b[i]);
+  for (size_t i = 0; i < a.size; ++i)
+    distance += count_bits(a.content[i] ^ b.content[i]);
   return distance;
+}
+int hamming_distance(const char *a, const char *b) {
+  return hamming_distance_buffer(buffer_from_string(a), buffer_from_string(b));
 }
 
 Buffer decode_base64(const char *encoded) {
@@ -66,6 +69,35 @@ char *read_file_as_oneline(const char *filename) {
   return result;
 }
 
+float keysize_score(const Buffer buffer, const int keysize) {
+  assert(keysize > 1);
+  assert(buffer.size > (keysize * 2));
+  Buffer a = buffer_new(keysize);
+  Buffer b = buffer_new(keysize);
+  for (size_t i = 0; i < keysize; ++i)
+    a.content[i] = buffer.content[i];
+  for (size_t i = keysize; i < keysize * 2; ++i)
+    b.content[i - keysize] = buffer.content[i];
+  int distance = hamming_distance_buffer(a, b);
+  free(a.content);
+  free(b.content);
+  return (float)distance / (float)keysize;
+}
+
+int find_keysize(const Buffer buffer) {
+  float min_score = 99999;
+  int keysize = 2;
+  for (size_t i = keysize; i <= 40; ++i) {
+    float score = keysize_score(buffer, i);
+    // printf("Keysize/Score - %2d - %f - %d\n", i, score, buffer.size);
+    if (score < min_score) {
+      min_score = score;
+      keysize = i;
+    }
+  }
+  return keysize;
+}
+
 int main(void) {
   // count bits test
   assert(5 == count_bits('O'));
@@ -81,27 +113,22 @@ int main(void) {
   assert(!strcmp("YWJjZGVm", encode_base64(buffer_from_string("abcdef"))));
   assert(!strcmp("YWJjZGVmZw==", encode_base64(buffer_from_string("abcdefg"))));
   // decode tests
-  puts(encode_base64(decode_base64("YWJjZA==")));
   assert(!strcmp("YWJj", encode_base64(decode_base64("YWJj"))));
   assert(!strcmp("YWJjZA==", encode_base64(decode_base64("YWJjZA=="))));
   assert(!strcmp("YWJjZGU=", encode_base64(decode_base64("YWJjZGU="))));
   assert(!strcmp("YWJjZGVm", encode_base64(decode_base64("YWJjZGVm"))));
   assert(!strcmp("YWJjZGVmZw==", encode_base64(decode_base64("YWJjZGVmZw=="))));
-  // decode file
-  char *contents = read_file_as_oneline("6.txt");
-  puts(contents);
-  puts("---");
-  puts(encode_base64(decode_base64(contents)));
-  assert(!strcmp(contents, encode_base64(decode_base64(contents))));
-  puts("---");
-  puts(encode_hex(decode_base64(
-      "SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t")));
   assert(equal_buffer(
       decode_base64(
           "SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t"),
       decode_hex("49276d206b696c6c696e6720796f757220627261696e206c696b652061207"
                  "06f69736f6e6f7573206d757368726f6f6d")));
+  // decode file
+  char *contents = read_file_as_oneline("6.txt");
+  printf("Keysize: %d\n", find_keysize(decode_base64(contents)));
+  assert(!strcmp(contents, encode_base64(decode_base64(contents))));
   FILE *f = fopen("6.txt", "r");
   fclose(f);
+  // free(contents);
   return 0;
 }
