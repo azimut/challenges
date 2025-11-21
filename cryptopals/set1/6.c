f9612adfd7eba2fe384c5cfc30f474ad17238623
azimut <azimut.github@protonmail.com>
2025-11-21 13:15:23 -0300

    crytopals: 1.6 finish (? base64 decode w/padding

--- cryptopals/set1/6.c
+++ cryptopals/set1/6.c
@@ -19,7 +19,6 @@ int hamming_distance(const char *a, const char *b) {
   return distance;
 }
 
-// TODO: account for == padding
 Buffer decode_base64(const char *encoded) {
   Buffer result = buffer_new((strlen(encoded) * 8 - (strlen(encoded) * 2)) / 8);
   size_t ridx = 0;
@@ -29,7 +28,15 @@ Buffer decode_base64(const char *encoded) {
       tmp = (tmp << 6) | decode_base64_char(encoded[j]);
     }
     result.content[ridx++] = (tmp >> (2 * 8)) & 0xFF;
+    if (encoded[i + 2] == '=') {
+      result.size -= 2;
+      break;
+    }
     result.content[ridx++] = (tmp >> (1 * 8)) & 0xFF;
+    if (encoded[i + 3] == '=') {
+      result.size -= 1;
+      break;
+    }
     result.content[ridx++] = (tmp >> (0 * 8)) & 0xFF;
   }
   return result;
@@ -60,11 +67,27 @@ char *read_file_as_oneline(const char *filename) {
 }
 
 int main(void) {
+  // count bits test
+  assert(5 == count_bits('O'));
+  assert(4 == count_bits('c'));
+  assert(3 == count_bits('C'));
+  assert(1 == count_bits(1));
+  // hamming distance test
+  assert(37 == hamming_distance("this is a test", "wokka wokka!!!"));
+  // encode tests
   assert(!strcmp("YWJj", encode_base64(buffer_from_string("abc"))));
   assert(!strcmp("YWJjZA==", encode_base64(buffer_from_string("abcd"))));
   assert(!strcmp("YWJjZGU=", encode_base64(buffer_from_string("abcde"))));
   assert(!strcmp("YWJjZGVm", encode_base64(buffer_from_string("abcdef"))));
   assert(!strcmp("YWJjZGVmZw==", encode_base64(buffer_from_string("abcdefg"))));
+  // decode tests
+  puts(encode_base64(decode_base64("YWJjZA==")));
+  assert(!strcmp("YWJj", encode_base64(decode_base64("YWJj"))));
+  assert(!strcmp("YWJjZA==", encode_base64(decode_base64("YWJjZA=="))));
+  assert(!strcmp("YWJjZGU=", encode_base64(decode_base64("YWJjZGU="))));
+  assert(!strcmp("YWJjZGVm", encode_base64(decode_base64("YWJjZGVm"))));
+  assert(!strcmp("YWJjZGVmZw==", encode_base64(decode_base64("YWJjZGVmZw=="))));
+  // decode file
   char *contents = read_file_as_oneline("6.txt");
   puts(contents);
   puts("---");
@@ -78,11 +101,6 @@ int main(void) {
           "SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t"),
       decode_hex("49276d206b696c6c696e6720796f757220627261696e206c696b652061207"
                  "06f69736f6e6f7573206d757368726f6f6d")));
-  assert(5 == count_bits('O'));
-  assert(4 == count_bits('c'));
-  assert(3 == count_bits('C'));
-  assert(1 == count_bits(1));
-  assert(37 == hamming_distance("this is a test", "wokka wokka!!!"));
   FILE *f = fopen("6.txt", "r");
   fclose(f);
   return 0;
